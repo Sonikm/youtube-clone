c3133c255f698566197e3c3e5fde4553cf3c1351
Sonikm <sonikm443@gmail.com>
2024-02-28 17:09:08 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
 
+import "./App.css";
+
 function App() {
   return (
-    <div className="App">
+    <div className="App font-bold">
      You Tube clone
     </div>
   );
 }
 
+
 export default App;
+
+/**
+ * Head
+ *   - HamBurger
+ *   - Logo
+ *   - SearchBar
+ *   - UserProfile
+ * Body
+ *    Sidebar
+ *      - MenuItems
+ *         - History
+ *         - Likes
+ *         - Saves
+ *         - Subscribs
+ *    MainContainer
+ *      - ButtonList
+ *      - VideoContainer
+ *         - VideoCard 
+ *    
+ */
+
+/**
+ * Head
+ * Body
+ *   Sidebar
+ *   WatchPage
+ *    - VideoContainer
+ *       - Video
+ *          - VideoInfo
+ *          - Comments
+ *       
+ */
